af2fe7c5c18988b7a87aadcd723e4b5bb00e708f
Georgy Parhomenko <g.parhomenko@itransition.com>
2021-09-26 13:41:46 +0300

    Create handlers for CLI commands

--- password_manager/cli/cli.py
+++ password_manager/cli/cli.py
@@ -1,9 +1,12 @@
 import configparser
+import logging
 
 import typer
+import pyperclip
 
-from cli.helpers import authenticated
+from cli.helpers import authenticated, db_session, prepare_bunches_for_output
 from main import CONFIG_PATH
+from services import BunchService, OutputableException
 
 app = typer.Typer()
 
@@ -22,6 +25,80 @@ def login(username: str, password: str):
 
 
 @app.command()
+@db_session
 @authenticated
-def hello(ctx: typer.Context) -> None:
-    typer.echo(f"Hello, {ctx.meta['username']}")
+def show_bunches(ctx: typer.Context) -> None:
+    """List bunches available for current user."""
+    try:
+        service = BunchService(ctx.meta['session'], ctx.meta['key'])
+        bunches = service.get_all_bunches(ctx.meta['user'])
+        output = prepare_bunches_for_output(bunches)
+        typer.echo(output)
+    except Exception as e:
+        typer.echo(f"Something went wrong. Check log file.")
+        logging.error(e)
+
+
+@app.command()
+@db_session
+@authenticated
+def search(ctx: typer.Context, name: str) -> None:
+    service = BunchService(ctx.meta['session'], ctx.meta['key'])
+    try:
+        bunch = service.find_bunch(name, ctx.meta['user'])
+        if not bunch:
+            typer.echo(f"No bunch named {name}")
+            raise typer.Exit()
+        output = prepare_bunches_for_output([bunch])
+        typer.echo(output)
+    except RuntimeError:
+        pass
+    except Exception as e:
+        typer.echo(f"Something went wrong. Check log file.")
+        logging.error(e)
+
+
+@app.command()
+@db_session
+@authenticated
+def copy(ctx: typer.Context, name: str) -> None:
+    service = BunchService(ctx.meta['session'], ctx.meta['key'])
+    try:
+        bunch = service.find_bunch(name, ctx.meta['user'])
+        if not bunch:
+            typer.echo(f"No bunch named: {name}")
+            raise typer.Exit()
+        copying_value = f"{bunch.login}\n{bunch.password}"
+        pyperclip.copy(copying_value)
+        typer.echo("Copied bunch info to clipboard.")
+    except Exception as e:
+        typer.echo(f"Something went wrong. Check log file.")
+        logging.error(e)
+
+
+@app.command()
+@db_session
+@authenticated
+def add(ctx: typer.Context, login: str, password: str, name: str) -> None:
+    service = BunchService(ctx.meta['session'], ctx.meta['key'])
+    try:
+        service.add_bunch(login, password, name, ctx.meta['user'])
+        typer.echo(f"Bunch for {name} was successfully added.")
+    except Exception as e:
+        typer.echo(f"Something went wrong. Check log file.")
+        logging.error(e)
+
+
+@app.command()
+@db_session
+@authenticated
+def delete(ctx: typer.Context, name: str) -> None:
+    service = BunchService(ctx.meta['session'], ctx.meta['key'])
+    try:
+        service.delete_bunch_by_name(name, ctx.meta['user'])
+        typer.echo("Bunch was deleted.")
+    except OutputableException as e:
+        typer.echo(e)
+    except Exception as e:
+        typer.echo(f"Something went wrong. Check log file.")
+        logging.error(e)
--- password_manager/cli/helpers.py
+++ password_manager/cli/helpers.py
@@ -1,20 +1,37 @@
+from __future__ import annotations
 import configparser
 import os
+import typing
 from functools import wraps
+from typing import List
 
 import typer
 
+from models import Session
 from main import CONFIG_PATH
+from security.security import get_key
+from services import AccountService
+
+if typing.TYPE_CHECKING:
+    from services import BunchObject
 
 
-def authenticated(func):
-    @wraps(func)
+def authenticated(f):
+    @wraps(f)
     def wrapper(ctx: typer.Context, *args, **kwargs):
-        if _is_config_exists():
-            ctx.meta['username'] = _get_username()
-            func(ctx, *args, **kwargs)
-        else:
+        if not _is_config_exists():
             typer.echo('First you should log in.\nType "login" command')
+            return
+        session = ctx.meta['session']
+        config = get_config()
+        username, password = config['default']['username'], config['default']['password']
+        service = AccountService(session)
+        if not (user := service.authorize_user(username, password)):
+            typer.echo("Wrong credentials.")
+            raise typer.Exit()
+        ctx.meta['user'] = user
+        ctx.meta['key'] = get_key(user.password)
+        f(ctx, *args, **kwargs)
     return wrapper
 
 
@@ -22,7 +39,25 @@ def _is_config_exists() -> bool:
     return os.path.exists(CONFIG_PATH)
 
 
-def _get_username() -> str:
+def get_config() -> configparser.ConfigParser:
     config = configparser.ConfigParser()
     config.read(CONFIG_PATH)
-    return config['default']['username']
+    return config
+
+
+def db_session(f):
+    @wraps(f)
+    def wrapper(ctx: typer.Context, *args, **kwargs):
+        with Session() as session:
+            ctx.meta['session'] = session
+            return f(ctx, *args, **kwargs)
+    return wrapper
+
+
+def prepare_bunches_for_output(bunches: List[BunchObject]) -> str:
+    output = [
+        f'{bunch.login} {bunch.password} {bunch.name}'
+        for bunch in bunches
+    ]
+    output = ["Login Password Name"] + output[:]
+    return "\n".join(output)
